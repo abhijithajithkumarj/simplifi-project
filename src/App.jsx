326cfbc94c9c9cb4b280f0e77d224b85036f29a1
ABHIJITH <aabhivishnu@gmail.com>
2024-08-01 02:11:03 +0530

    add router and card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Explore } from "./Components/explor/Explore";
+import { Home } from "./Components/home/Home";
+import { Card } from "./Components/card/Card";
+import { Nav } from "./Components/nav/Nav";
+import { Category } from "./Components/category/Category";
+import { Header } from "./Components/header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +15,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Explore/>}/>
+          <Route path="home" element={<Home/>}/>
+          <Route path="card" element={<Card/>}/>
+          <Route path="nave" element={<Nav/>}/>
+          <Route path="category" element={<Category/>}/>
+          <Route path="header" element={<Header/>}/>
 
         </Routes>
       </BrowserRouter>
---
+++ src/Components/nav/Nav.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <div>
+      <div className="fixed z-50 w-full  max-w-lg -translate-x-1/2 bg-white border border-black rounded-full bottom-0  left-1/2  h-24">
+      
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto"> 
+
+          <button type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group">
+          <i className="fa-solid fa-house-chimney  text-2xl opacity-85"></i>
+            <span className="sr-only">Home</span>
+          </button>
+
+          <button type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group">
+          <i className="fa-solid fa-ticket-simple  text-2xl  opacity-85"></i>
+            <span className="sr-only">Home</span>
+          </button>
+
+          <button type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group">
+          <i className="fa-solid fa-heart  text-2xl  opacity-85"></i>
+            <span className="sr-only">Home</span>
+          </button>
+
+          <button type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group">
+          <i className="fa-solid fa-user text-2xl  opacity-85"></i>
+            <span className="sr-only">Home</span>
+          </button>
+
+       
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
