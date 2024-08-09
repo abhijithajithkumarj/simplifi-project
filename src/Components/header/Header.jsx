1cfe04d92ba15fb9803e345aba47de0dde0bd02d
ABHIJITH <aabhivishnu@gmail.com>
2024-08-09 21:35:38 +0530

    add header

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -17,7 +17,7 @@ export const Header = () => {
               <form className="ml-2">
                 <select
                   id="countries"
-                  className="bg-gray-50 bg-opacity-80 border text-sm rounded-lg block lg:p-2.5 dark:bg-opacity-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black0 sm:w-50 md:w-50 lg:w-60 w-[70px] mt-4 text-black appearance-auto "
+                  className="bg-gray-50 bg-opacity-80 border text-sm rounded-lg block lg:p-2.5 dark:bg-opacity-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black0 sm:w-50 md:w-50 lg:w-60 w-[77px] mt-4 text-black appearance-auto pt-1 pb-3"
                 >
                   <option selected>Aspen,USA </option>
                   <option value="CA"> Canada</option>
